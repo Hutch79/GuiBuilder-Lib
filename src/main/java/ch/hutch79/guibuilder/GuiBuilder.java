6331ac770f3ab11c7308bc151d949ab37f2363ea
Hutch79 <andrin.monn@gmail.com>
2023-06-30 08:56:43 +0200

    Added Java Docs and check in createGui if GUI name already exists

--- src/main/java/ch/hutch79/guibuilder/GuiBuilder.java
+++ src/main/java/ch/hutch79/guibuilder/GuiBuilder.java
@@ -14,19 +14,40 @@ public class GuiBuilder {
     private static final HashMap<String, StoreGui> guis = new HashMap<>();
 
     public void createGui(String name, int guiSize) {
+        if (guis.containsKey(name)) {
+            Bukkit.getLogger().warning("A GUI called '" + name + "' does already exist!");
+            return;
+        }
         ItemStack[] itemStack = new ItemStack[guiSize];
         guis.put(name, new StoreGui(name, guiSize, itemStack));
     }
 
-    public void addItem(String gui, int slot, ItemStack item) {
+    /**
+     * Adds an Item in a given GUI on a give Slot.
+     * @param gui Name of the GUI
+     * @param slot slot where the item should be placed in
+     * @param item Item to add on slot
+     */
+    public void setItem(String gui, int slot, ItemStack item) {
         guis.get(gui).getItems()[slot] = item;
     }
 
-    public void setName(String gui, String name) {
-        guis.get(gui).setName(name);
+    /**
+     * Set the Title which is seen on the very top of your GUI.
+     * @param gui Name of GUI
+     * @param title GUI Title (Visible in-game)
+     */
+    public void setTitle(String gui, String title) {
+        guis.get(gui).setName(title);
     }
 
 
+    /**
+     * Opens a GUI for player.
+     * If player already had the GUI opened, the same GUI will be loaded again
+     * @param player Player to open the GUI
+     * @param gui Which GUI to open
+     */
     public void openGui(Player player, String gui) {
         UUID uuid = player.getUniqueId();
         if (playerGuis.containsKey(uuid) && playerGuis.get(uuid).containsKey(gui)) {
